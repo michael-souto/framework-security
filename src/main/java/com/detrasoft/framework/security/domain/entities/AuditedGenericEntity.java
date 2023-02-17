3ade059d4df99fa05cb94aa87e6ec4915ec08015
Michael Souto <michaelsouto@outlook.com>
2023-02-16 22:54:40 -0300

    Add entities for security

---
+++ src/main/java/com/detrasoft/framework/security/domain/entities/AuditedGenericEntity.java
@@ -0,0 +1,38 @@
+package com.detrasoft.framework.security.domain.entities;
+
+import com.detrasoft.framework.crud.entities.Audit;
+import com.detrasoft.framework.crud.entities.GenericEntity;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.persistence.Embedded;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.Instant;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class AuditedGenericEntity extends GenericEntity {
+
+    @Embedded
+    private com.detrasoft.framework.crud.entities.Audit audit = new Audit();
+
+    @PrePersist
+    public void prePersist() {
+        audit.setCreatedAt(Instant.now());
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            audit.setUserCreated(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        audit.setUpdatedAt(Instant.now());
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            audit.setUserUpdated(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
+    }
+}
